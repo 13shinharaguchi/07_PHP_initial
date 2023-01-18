835934d3e5c3865d23184b53d22805d2c2d7b0b7
13shinharaguchi <shin13usa@icloud.com>
2023-01-18 09:32:41 +0900

    add

--- input.php
+++ input.php
@@ -12,6 +12,7 @@
   <input type="checkbox" name="emotions[]" value="sad">かなしい
   <input type="checkbox" name="emotions[]" value="fun">たのしい
   <input type="submit" value="送信">
+  <div>ついか</div>
   <div>てすと01</div>
   <div>テスト02、pushした後に追加してまたupdateする</div>
   <div>追加できることはわかった</div>
